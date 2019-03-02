439e08036f57e828cbf25adcea2586d5c349b21c
menghh <595004514@qq.com>
2019-03-02 15:06:32 +0800

    using of self-defining library

--- library/useLibrary/src/app.js
+++ library/useLibrary/src/app.js
@@ -1,6 +1,9 @@
-import '../../myLibrary/dist/css/styles.css'
+//import '../../myLibrary/dist/css/styles.css'
+import 'first-library-of-mcc/dist/css/styles.css'
+
 import './app.css'
 
-var app = require('../../myLibrary/dist/library.app.js')
+import * as app from 'first-library-of-mcc/dist/library.app.js';
+//var app = require('../../myLibrary/dist/library.app.js')
 
 console.log(app.print())
--- library/useLibrary/src/index.js
+++ library/useLibrary/src/index.js
@@ -3,10 +3,10 @@ import './index.css'
 require('lodash');  // 官方demo用了这个
 // 在library的webpack.config.js中，若没有通过ProvidePlugin插件，由webpack去动态加载lodash，则在此需要引入lodash
 
-// import * as webpackNumbers from 'first-library-of-mcc';
+import * as webpackNumbers from 'first-library-of-mcc';
 
 // var webpackNumbers = require('../../myLibrary/dist/webpack-numbers.js')
-var webpackNumbers = require('../../myLibrary/dist/library.main.js')
+// var webpackNumbers = require('../../myLibrary/dist/library.main.js')
 // var webpackNumbers = require('first-library-of-mcc')
 
 console.log(webpackNumbers.numtoword(4))
